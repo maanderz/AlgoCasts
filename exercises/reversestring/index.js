d44458b7ea926ba573e145e1548f09d0aa4a6ec9
Mandy Arens <mandyfung93@gmail.com>
2020-04-08 16:21:38 -0600

    did reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// SOLUTION #3 
+function reverse(str) {
+  debugger;
+  return str.split('').reduce((reversed, char) => char + reversed, '');
+}
+
+// SOLUTION #2
+// function reverse(str) {
+//   let answer = ''
+//   // CLASSIC FOR LOOP
+//   // for (let i = str.length -1; i >= 0; i--){
+//   //   answer += str[i]
+//   // } 
+//   // return answer
+
+//   // FOR OF LOOP
+//   for (let character of str) {
+//     answer = character + answer
+//   }
+//   return answer
+// }
+
+
+
+// SOLUTION #1 
+// function reverse(str) {
+//   let split = str.split('')
+//   return split.reverse().join('');
+// }
 
 module.exports = reverse;
