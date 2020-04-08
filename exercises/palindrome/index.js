c9ece24b688aa9ec8d75633ad9c818b98395db3f
Mandy Arens <mandyfung93@gmail.com>
2020-04-08 17:05:14 -0600

    did palindrome question

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,50 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// SOLUTION #1 - 0.737s
+// function palindrome(str) {
+//   // reverse string
+//   let reverse = str.split('').reduce((inProgress, char) => {
+//     return char + inProgress 
+//   }, '')
+  
+//   // compare two strings
+//   return reverse === str;
+// }
+
+
+
+// SOLUTION #2 - 0.772s
+// function palindrome(str) {
+//   // reverse string
+//   let reverse = str.split('').reverse().join('')
+  
+//   // compare two strings
+//   if (reverse === str){
+//     return true; 
+//   } else {
+//     return false
+//   }
+// }
+
+
+
+// SOLUTION #3 - 1.577 s
+// function palindrome(str){
+//   let reverse = '';
+//   for (let i = str.length -1; i >= 0; i--){
+//     reverse += str[i]
+//   }
+//   return (reverse === str ? true : false);
+// }
+
+
+
+// SOLUTION #4 
+function palindrome(str){
+  return str.split('').every((char, i) =>{
+    return char === str[str.length - i - 1];
+  })
+}
 
 module.exports = palindrome;
