4b9d865384e7a617865db7f62b85b99835c1c017
Mandy Arens <mandyfung93@gmail.com>
2020-04-09 16:00:20 -0600

    did maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {};
+  let max = 0; 
+  let maxChar = '';
+
+  for (let char of str) {
+    (!obj[char] ? obj[char] = 1 : obj[char]++) 
+    // char = a , p 
+    // obj[char] = key 
+  }
+
+  for (let char in obj){
+    if (obj[char] > max){
+      max = obj[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+  
+
+  // for (let i = 0; i < str.length; i++){
+  //   (!obj[str[i]] ? obj[str[i]] = 1 : obj[str[i]] += 1)
+  // }
+
+  // return Object.keys(obj).reduce((a, b) => obj[a]> obj[b] ? a : b);
+}
+
+console.log(maxChar('aaaappppppe'))
 
 module.exports = maxChar;
